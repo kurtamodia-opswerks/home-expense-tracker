f64cca5daee10c558eb92b4dc87ccd6e84b307b2
kurtamodia-opswerks <km.amodia@ssed-academy.opswerks.com>
2025-09-11 22:10:50 +0800

    refactor: change navbar component into server component

--- components/navigation/Navbar.tsx
+++ components/navigation/Navbar.tsx
@@ -1,7 +1,4 @@
-"use client";
-
 import Link from "next/link";
-import { revalidateData } from "@/app/actions/revalidate";
 import { buttonVariants } from "../ui/button";
 import {
   RegisterLink,
@@ -10,26 +7,12 @@ import {
 } from "@kinde-oss/kinde-auth-nextjs/components";
 import { User, CreditCard, Users, House } from "lucide-react";
 
-import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
-import { useEffect, useState } from "react";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export function Navbar() {
-  const { getUser, isAuthenticated } = useKindeBrowserClient();
-  const [user, setUser] = useState(getUser());
+export async function Navbar() {
+  const { getUser } = getKindeServerSession();
 
-  useEffect(() => {
-    const checkUser = async () => {
-      const currentUser = await getUser();
-      setUser(currentUser);
-
-      // If user just logged in, revalidate cache
-      if (currentUser) {
-        await revalidateData();
-      }
-    };
-
-    checkUser();
-  }, [isAuthenticated]);
+  const user = await getUser();
 
   return (
     <div className="flex items-center justify-between container mx-auto px-4 md:px-8 py-4">
