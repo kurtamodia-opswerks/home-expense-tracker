b931058976ef3688ce636640df4042cf992206d9
kurtamodia-opswerks <km.amodia@ssed-academy.opswerks.com>
2025-09-11 22:31:52 +0800

    refactor: centralize transaction share objects in the main page.tsx and then pass it as props to child components

--- app/transaction-shares/page.tsx
+++ app/transaction-shares/page.tsx
@@ -1,11 +1,16 @@
 // app/transaction-shares/page.tsx
 import TransactionShareList from "@/components/transactionShares/TransactionShareList";
+import { getTransactionShares } from "../data/transaction/get-transaction-shares";
+import { getOrCreateUser } from "../data/user/get-or-create-user";
 
 export default async function TransactionShares() {
+  const shares = await getTransactionShares();
+  const currentUser = await getOrCreateUser();
+
   return (
     <div className="flex flex-col items-center gap-6 p-6">
       <h1 className="text-3xl font-bold">Transaction Shares</h1>
-      <TransactionShareList />
+      <TransactionShareList shares={shares} user={currentUser} />
     </div>
   );
 }
--- components/transactionShares/TransactionShareList.tsx
+++ components/transactionShares/TransactionShareList.tsx
@@ -1,11 +1,28 @@
 // app/components/transactions/TransactionSharesListWrapper.tsx
 import TransactionSharesListClient from "@/components/transactionShares/TransactionSharesListClient";
-import { getTransactionShares } from "@/app/data/transaction/get-transaction-shares";
-import { getOrCreateUser } from "@/app/data/user/get-or-create-user";
+import type { SelectTransactionShare } from "@/db/schema";
+import type { SelectUser } from "@/db/schema";
 
-export default async function TransactionSharesListWrapper() {
-  const shares = await getTransactionShares();
-  const user = await getOrCreateUser();
+interface TransactionShare {
+  id: number;
+  transactionDate: string | null;
+  transactionDescription: string | null;
+  debtorId: number;
+  debtorName: string | null;
+  toPay: number;
+  paid: number | null;
+  receiverId: number;
+  receiverName: string;
+}
 
+interface TransactionShareListProps {
+  shares: TransactionShare[];
+  user: SelectUser | null;
+}
+
+export default async function TransactionSharesListWrapper({
+  shares,
+  user,
+}: TransactionShareListProps) {
   return <TransactionSharesListClient shares={shares} currentUser={user} />;
 }
