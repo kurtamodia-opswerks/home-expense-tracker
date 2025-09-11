0b0fd3e2eaeed35473e269b2ddd06b2fe0d36edd
kurtamodia-opswerks <km.amodia@ssed-academy.opswerks.com>
2025-09-11 08:19:57 +0800

    refactor: refactor HomeForm to use react-hook-form and zod for easier dx and validation

--- components/home/HomeForm.tsx
+++ components/home/HomeForm.tsx
@@ -1,67 +1,81 @@
 // app/components/home/HomeForm.tsx
 "use client";
 
-import { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "../ui/button";
+import { InsertHome } from "@/db/schema";
+
+const schema = z.object({
+  name: z.string().min(1),
+  address: z.string().min(1),
+});
 
 interface HomeFormProps {
   onSuccess: () => void;
   userId: number;
 }
 
+type FormData = z.infer<typeof schema>;
+
 export default function HomeForm({ onSuccess, userId }: HomeFormProps) {
-  const [name, setName] = useState("");
-  const [address, setAddress] = useState("");
-  const [error, setError] = useState("");
-
-  async function handleSubmit(e: React.FormEvent) {
-    e.preventDefault();
-    setError("");
-
-    const res = await fetch("/api/homes", {
-      method: "POST",
-      body: JSON.stringify({ name, address, userId }),
-      headers: { "Content-Type": "application/json" },
-    });
-
-    if (res.ok) {
-      setName("");
-      setAddress("");
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors, isSubmitting },
+  } = useForm<FormData>({
+    defaultValues: { name: "Sunset", address: "" },
+    resolver: zodResolver(schema),
+  });
+  const onSubmit: SubmitHandler<InsertHome> = async (data) => {
+    try {
+      await fetch("/api/homes", {
+        method: "POST",
+        body: JSON.stringify({ ...data, userId }),
+        headers: { "Content-Type": "application/json" },
+      });
+      throw new Error();
       onSuccess();
-    } else {
-      const data = await res.json();
-      setError(data.error || "Something went wrong");
+    } catch (error) {
+      setError("root", { message: "Failed to create home" });
     }
-  }
+  };
 
   return (
     <form
-      onSubmit={handleSubmit}
       className="flex flex-col gap-2 w-full max-w-md"
+      onSubmit={handleSubmit(onSubmit)}
     >
       <input
+        {...register("name")}
         type="text"
         placeholder="Home Name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
         className="border p-2 rounded"
-        required
       />
+      {errors.name && (
+        <span className="text-red-500">{errors.name.message}</span>
+      )}
       <input
+        {...register("address")}
         type="text"
         placeholder="Home Address"
-        value={address}
-        onChange={(e) => setAddress(e.target.value)}
         className="border p-2 rounded"
-        required
       />
+      {errors.address && (
+        <span className="text-red-500">{errors.address.message}</span>
+      )}
       <Button
+        disabled={isSubmitting}
         className="bg-blue-500 text-white px-4 py-2 rounded"
         type="submit"
       >
-        Create Home
+        {isSubmitting ? "Loading" : "Create Home"}
       </Button>
-      {error && <p className="text-red-500">{error}</p>}
+      {errors.root && (
+        <span className="text-red-500">{errors.root.message}</span>
+      )}
     </form>
   );
 }
