e21f25de50275fd0f320a60f13d5941215b84a2a
kurtamodia-opswerks <km.amodia@ssed-academy.opswerks.com>
2025-09-13 07:41:12 +0800

    feat: add tooltips to navigation links for clarity and better ux

--- components/navigation/NavLinks.tsx
+++ components/navigation/NavLinks.tsx
@@ -3,29 +3,66 @@ import { User, CreditCard, Users, House } from "lucide-react";
 
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
 export async function NavLinks() {
   const { getUser } = getKindeServerSession();
-
   const user = await getUser();
 
+  if (!user) return null;
+
   return (
-    <>
-      {user && (
-        <>
-          <Link href="/home">
-            <House />
-          </Link>
-          <Link href="/users">
-            <User />
-          </Link>
-          <Link href="/transactions">
-            <CreditCard />
-          </Link>
-          <Link href="/transaction-shares">
-            <Users />
-          </Link>
-        </>
-      )}
-    </>
+    <TooltipProvider delayDuration={100}>
+      <div className="flex gap-4">
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Link href="/home">
+              <House />
+            </Link>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Home</p>
+          </TooltipContent>
+        </Tooltip>
+
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Link href="/users">
+              <User />
+            </Link>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Users</p>
+          </TooltipContent>
+        </Tooltip>
+
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Link href="/transactions">
+              <CreditCard />
+            </Link>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Transactions</p>
+          </TooltipContent>
+        </Tooltip>
+
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Link href="/transaction-shares">
+              <Users />
+            </Link>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>Transaction Shares</p>
+          </TooltipContent>
+        </Tooltip>
+      </div>
+    </TooltipProvider>
   );
 }
