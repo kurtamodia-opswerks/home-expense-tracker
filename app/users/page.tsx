013401b2cfcc6dc273ad876baf250e79f3d490da
kurtamodia-opswerks <km.amodia@ssed-academy.opswerks.com>
2025-09-13 07:42:40 +0800

    fix: fix title of users page

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -7,7 +7,7 @@ import { Suspense } from "react";
 export default async function Users() {
   return (
     <div className="flex flex-col items-center gap-6 p-6">
-      <h1 className="text-3xl font-bold">Home Expense Tracker App</h1>
+      <h1 className="text-3xl font-bold">Users</h1>
       <Suspense fallback={<UserInfoSkeleton />}>
         <UserInfo />
       </Suspense>
