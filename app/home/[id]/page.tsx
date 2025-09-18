4a68a7934b23475b7cf9b4f6882c126530bdc376
kurtamodia-opswerks <km.amodia@ssed-academy.opswerks.com>
2025-09-18 10:36:08 +0800

    lint: fix eslint errors

--- app/home/[id]/page.tsx
+++ app/home/[id]/page.tsx
@@ -2,7 +2,7 @@ import HomeAnalytics from "@/components/analytics/HomeAnalytics";
 import HomeAnalyticsSkeleton from "@/components/home/loading/HomeAnalyticsSkeleton";
 import { Suspense } from "react";
 
-export default function HomePage({ params }: { params: { id: string } }) {
+export default function HomePage({ params }: any) {
   const homeId = parseInt(params.id, 10);
 
   return (
