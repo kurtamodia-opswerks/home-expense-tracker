7bc62eafd28d567e72d0c9c1defc9d003868d887
kurtamodia-opswerks <km.amodia@ssed-academy.opswerks.com>
2025-09-18 13:40:33 +0800

    fix: fix page props for dynamic home page

--- app/home/[id]/page.tsx
+++ app/home/[id]/page.tsx
@@ -1,15 +1,15 @@
-import { type ResolvingMetadata } from "next";
 import HomeAnalytics from "@/components/analytics/HomeAnalytics";
 import HomeAnalyticsSkeleton from "@/components/home/loading/HomeAnalyticsSkeleton";
 import { Suspense } from "react";
 
-export default function HomePage({ params }: { params: { id: string } }) {
-  const homeId = Number(params.id);
+export default async function HomePage(props: PageProps<"/home/[id]">) {
+  const { id } = await props.params;
+  console.log(id);
 
   return (
     <div className="p-6">
       <Suspense fallback={<HomeAnalyticsSkeleton />}>
-        <HomeAnalytics homeId={homeId} />
+        <HomeAnalytics homeId={Number(id)} />
       </Suspense>
     </div>
   );
